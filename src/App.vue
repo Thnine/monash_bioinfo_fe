1f3d1a6620c40a80e33d0d8883e0d9d093d5a6f7
Thnine <775903190@qq.com>
2024-06-04 20:31:21 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <test1/>
+  </div>
+</template>
+
+<script>
+import test1 from "@/components/test1.vue"
+
+
+export default {
+  name: 'App',
+  components: {
+    test1
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
